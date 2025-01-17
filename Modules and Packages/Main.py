d8d2c8c20b3255108caae3bc1ef566bb01664743
Rishav Rathor <156896185+Rishav-Rathor@users.noreply.github.com>
2025-01-17 09:56:05 +0530

    Add files via upload

---
+++ Modules and Packages/Main.py
@@ -0,0 +1,28 @@
+from Maths.Simple import sub
+
+print(sub(2,3))
+
+
+
+
+
+
+
+
+
+
+
+
+# import Simple
+# import Complex
+
+# print(Simple.add(10,5))
+# print(Complex.cube(5))
+
+# ### Or
+
+# from Simple import add
+# from Complex import cube
+
+# print(add(10,5))
+# print(cube(5))
---
+++ Modules and Packages/Simple.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return(a+b)
+
+def sub(a,b):
+    print(a-b)
+
+def mul(a,b):
+    print(a*b)
+
+def div(a,b):
+    print(a/b)
+
+print("Hello")
